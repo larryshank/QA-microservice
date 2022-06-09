9772b3d6df22d0076325c0b997d90237572f57e1
larryshank <larry113@gmail.com>
2022-06-09 17:44:47 -0600

    Added answersList query and got a working listQuestions Query

--- model/dbQueries/dbqueries.js
+++ model/dbQueries/dbqueries.js
@@ -4,33 +4,20 @@ const { Pool } = require('pg');
 
 const pool = new  Pool();
 
-// const answersList = (question_id, page, count) => {
-//   count < 1 ? count = 1 : count;
-//   page < 1 ? page = 1 : page;
-//   const start = count * (page - 1);
+const answersList = (question_id, page, count) => {
+  count < 1 ? count = 1 : count;
+  page < 1 ? page = 1 : page;
+  const start = count * (page - 1);
 
-//   const ans = 'SELECT id, body, date_written, answerer_name, answerer_email, reported, helpful WHERE question_id = $1 LIMIT $2 OFFSET $3';
+  const ans =  `SELECT *,
+  (SELECT JSON_agg(answers_photos) FROM answers_photos WHERE answers.id = answers_photos.answer_id) AS PICS
+  FROM answers
+  WHERE answers.question_id = $1 LIMIT $2 OFFSET $3`;
 
-//   const args = [question_id, page, count];
+  const args = [question_id, count, start];
 
-//   return pool.query(ans, args);
-// }
-
-// const listQuestions = (product_id, page, count) => {
-//   count < 1 ? count = 1 : count;
-//   page < 1 ? page = 1 : page;
-//   const start = count * (page - 1);
-
-//   const quest = 'SELECT id, body, date_written, asker_name, asker_email, helpful, reported FROM questions WHERE product_id=$1 LIMIT $2 OFFSET $3';
-
-//   const args = [product_id, count, start]
-
-//   return pool.query(quest, args);
-// };
-
-// const quest = 'EXPLAIN ANALYZE SELECT *, (SELECT JSON_object_agg(answers.id, row_to_json(answers)) from answers WHERE questions.id = answers.question_id) answers FROM questions WHERE product_id=$1 LIMIT $2 OFFSET $3';
-
-// (SELECT array_agg(row_to_json(answers_photos)) from answers_photos WHERE answers_photos.answer_id = answers.id) photos
+  return pool.query(ans, args);
+}
 
 const listQuestions = (product_id, page, count) => {
   count < 1 ? count = 1 : count;
@@ -38,18 +25,23 @@ const listQuestions = (product_id, page, count) => {
   const start = count * (page - 1);
 
   const quest = `SELECT *,
-                    (SELECT JSON_object_agg(answers.id, row_to_json(answers))
+  (SELECT JSON_object_agg(fullans.id, fullans)
+  FROM
+    (SELECT *,
+      (SELECT JSON_agg(answers_photos) FROM answers_photos WHERE answers.id = answers_photos.answer_id) AS PICS
+    FROM answers
+    WHERE questions.id = answers.question_id) AS fullans) AS answers
+  FROM questions WHERE product_id=$1 LIMIT $2 OFFSET $3`;
 
-                    from answers WHERE questions.id = answers.question_id) answers
 
-                FROM questions WHERE product_id=$1 LIMIT $2 OFFSET $3`;
-
-  const args = [product_id, count, start]
+  const args = [product_id, count, start];
 
   return pool.query(quest, args);
 };
 
+
+
 module.exports = {
   listQuestions,
-  // answersList,
+  answersList,
 };
--- routes/qa/qa.js
+++ routes/qa/qa.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const {listQuestions} = require('../../model/dbQueries/dbqueries.js')
+const {listQuestions, answersList} = require('../../model/dbQueries/dbqueries.js')
 
 router.get('/', (req, res) => {
   console.log(req.query);
   const product_id = req.query.product_id;
   const page = req.query.page || 1;
-  const count = req.query.count || 5
+  const count = req.query.count || 5;
   listQuestions(product_id, page, count)
     .then(questions => res.send(questions))
+    .catch(error => console.log(error));
+});
+
+router.get('/:question_id/answers', (req, res) => {
+  console.log(req.params);
+  const product_id = req.params.question_id;
+  const page = req.query.page || 1;
+  const count = req.query.count || 5;
+  answersList(product_id, page, count)
+    .then(answers => res.send(answers))
     .catch(error => console.log(error));
 });
 
