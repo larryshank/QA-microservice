82af5a6725d57266b2a4a2f2a4dd8898cb4f11ce
larryshank <larry113@gmail.com>
2022-06-13 20:00:05 -0600

    Please just work

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const port = 3000;
 const path = require('path');
 
 app.use(express.json());
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
 
 app.use('/qa', router);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
